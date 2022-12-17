382a7915bca2c6ca8bdc8120e5716b0b7f89fc4d
emeibech <emeibech@gmail.com>
2022-12-17 22:03:11 +0800

    Render and animate images and loop through them indefinitely

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import './styles.css';
 import { imageSlider } from "./modules/imageSlider";
 import { navigationArrows } from "./modules/sliderNavigation";
 
-document.addEventListener('DOMContentLoaded', navigationArrows);
-document.addEventListener('DOMContentLoaded', imageSlider.slider);
+document.addEventListener('DOMContentLoaded', () => {
+    navigationArrows();
+    imageSlider.renderImage(0);
+    imageSlider.autoSlider();
+});
--- src/modules/imageSlider.js
+++ src/modules/imageSlider.js
@@ -1,48 +1,76 @@
-import image0Source from "../images/img0.jpg";
-import image1Source from "../images/img1.jpg";
-import image2Source from "../images/img2.jpg";
-import image3Source from "../images/img3.jpg";
-import image4Source from "../images/img4.jpg";
-import image5Source from "../images/img5.jpg";
-import image6Source from "../images/img6.jpg";
+import imageSource0 from '../images/img0.jpg';
+import imageSource1 from '../images/img1.jpg';
+import imageSource2 from '../images/img2.jpg';
+import imageSource3 from '../images/img3.jpg';
+import imageSource4 from '../images/img4.jpg';
+import imageSource5 from '../images/img5.jpg';
+import imageSource6 from '../images/img6.jpg';
 
 export const imageSlider = (() => {
     const imageSource = [
-        image0Source,
-        image1Source,
-        image2Source,
-        image3Source,
-        image4Source,
-        image5Source,
-        image6Source
+        imageSource0,
+        imageSource1,
+        imageSource2,
+        imageSource3,
+        imageSource4,
+        imageSource5,
+        imageSource6
     ];
 
-    const imageNodes = [];
+    const renderImage = (imageNum) => {
+        const imageContainer = document.querySelector('.image-container');
+        const image = document.createElement('img');
+        image.src = imageSource[imageNum];
+        image.setAttribute('height', '720px');
+        image.setAttribute('width', '1280px');
+        image.setAttribute('data-image', `${imageNum}`);
+        imageContainer.appendChild(image);
+    }
 
-    const createImages = (() => {
-        imageSource.forEach((item) => {
-            const image = document.createElement('img');
-            image.src = item;
-            image.setAttribute('height', '720px');
-            image.setAttribute('width', '1280px');
-            imageNodes.push(image);
-        });
-    })();
+    const removeImage = (image) => {
+        setTimeout(() => {
+            const imageContainer = document.querySelector('.image-container');
+            image.nextElementSibling.style.transition = 'none';
+            image.nextElementSibling.style.translate = '0%';
+            imageContainer.removeChild(image);
+        }, 550);
+    }
 
-    const slider = () => {
-        const imageSlider = document.querySelector('.image-slider');
-        const imageContainer = document.createElement('div');
-        imageContainer.classList.add('image-container');
-        
-        imageNodes.forEach((item) => {
-            imageContainer.appendChild(item);
-        });
+    const animateRight = () => {
+        setTimeout(() => {
+            const image = document.querySelector('.image-container > img:last-child');
+            image.classList.add('animate-right');
+        }, 50);
+    }
 
-        imageSlider.appendChild(imageContainer);
+    const autoSlider = () => {
+        setTimeout(() => {
+            const currentImage = document.querySelector('.image-container > img');
+
+            // Get data-image attribute value of currentImage and convert it to a number type
+            const imageNum = Number(currentImage.getAttribute('data-image'));
+
+            // This logic makes it so once the slider has rendered and animated
+            //  all the image from imageSource array it will render the first image again
+            if (imageNum < imageSource.length - 1) {
+                // Renders new image, animate it, remove previous image, and call autoslider again
+                renderImage(imageNum + 1);
+                animateRight();
+                removeImage(currentImage);
+                autoSlider();
+            } else {
+                renderImage(0);
+                animateRight();
+                removeImage(currentImage);
+                autoSlider();
+            }
+
+        }, 5000);
     }
 
     return {
-        slider,
+        renderImage,
+        autoSlider
     }
 
 })();
